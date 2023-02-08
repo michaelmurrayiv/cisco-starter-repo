bc1392e850838de3e8687986d98650c1e35d5be6
Michael Murray <michael.murray.iv@gmail.com>
2023-02-07 19:24:56 -0800

    Created Title Banner, Exhibit Component

---
+++ sextant/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Banner from './Banner';
+import Exhibit from './Exhibit';
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <Banner bannerText="Sextant" />
+        <Exhibit name="Exhibit 1"></Exhibit>
+        <Exhibit name="Exhibit 2"></Exhibit>
+        <Exhibit name="Exhibit 3"></Exhibit>
+      </div>
+    );
+  }
+}
+
+
+export default App;
